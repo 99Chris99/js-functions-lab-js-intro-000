2612c8020bc6977dbf0ffa54349eaa903be107e7
Chris Hodge <chrishodge87@gmail.com>
2019-06-18 11:07:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ return "Happy holidays!"
 function happyHolidaysTo(name) {
   return "Happy holidays, " + `${name}` + "!"
 }
+
+happyHolidayTo(holiday, name) {
+  return "Happy " + `${holiday},${name}` +"!"
+}
